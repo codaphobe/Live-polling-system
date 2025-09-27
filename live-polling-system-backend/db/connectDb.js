2eaea23bb34f49cdbedac0d88db9f0e00e647d08
codephobe <kutakanakerisanjay@gmail.com>
2025-09-27 21:27:33 +0530

    MongoDB connection

--- live-polling-system-backend/db/connectDb.js
+++ live-polling-system-backend/db/connectDb.js
@@ -4,13 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // Replace this with your actual Atlas connection string
-console.log("MongoDB URI from env:", process.env);
-const mongoURI = process.env.MONGO_URI;
-console.log("Connecting to MongoDB with URI:", process.env.MONGO_URI);
 
-mongoose.connect(mongoURI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+const mongoURI = process.env.MONGO_URI;
+
+
+mongoose.connect(mongoURI);
 
 
 const db = mongoose.connection;
